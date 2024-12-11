00302a9439c40a526bfa513ed6db732243d56ba5
lok419 <cheungclok419@gmail.com>
2024-12-12 02:12:27 +0800

    optimize runtime of RSI and Double7 model

--- strategy_v2/Strategy/MVO/AlphaModel/Double7.py
+++ strategy_v2/Strategy/MVO/AlphaModel/Double7.py
@@ -2,7 +2,6 @@ from pandas.core.api import DataFrame as DataFrame
 import numpy as np
 from datetime import datetime
 from strategy_v2.Strategy.MVO import AlphaModel
-from utils.data_helper import add_bday
 
 
 class Double7(AlphaModel):      
@@ -18,26 +17,23 @@ class Double7(AlphaModel):
     def __str__(self) -> str:
         return f"Double({self.periods})"
     
-    def expected_return(self, pos_date: datetime) -> np.ndarray:
+    def preprocess_data(self, data):
+        super().preprocess_data(data)
 
-        lookback_end = add_bday(pos_date, -1)
-        expected_ret = []
+        df = self.data['px']           
 
-        df = self.data['px']
-        df = df[:lookback_end]
-        df = df.tail(200)        
-
-        assert max(df.index) < pos_date, 'Optimization has lookahead bias'             
-
-        df_close = df['Close']                
+        # signals on T is based on T-1 Close and before
+        df_close = df['Close'].shift(1)
+        
         df_200ma = df_close.rolling(200).mean()        
         df_7d_low = df_close.rolling(self.periods).min()
         df_7d_high = df_close.rolling(self.periods).max()
+        df_ret = df_close.pct_change().rolling(self.periods).mean().abs().clip(0, 1)
 
-        df_entry = 1 * ((df_close == df_7d_low) & (df_close > df_200ma))
+        df_entry = df_ret * ((df_close == df_7d_low) & (df_close > df_200ma))
         df_exit = -1 * ((df_close == df_7d_high) | (df_close < df_200ma))
 
-        df_sig = df_entry + df_exit
+        df_sig = (df_entry + df_exit).fillna(0)
         df_pos = df_sig.copy()
 
         # re-create the position based on signals
@@ -45,7 +41,10 @@ class Double7(AlphaModel):
             df_pos.iloc[i] = df_pos.iloc[i] + df_pos.iloc[i-1]
             df_pos.iloc[i] = np.clip(df_pos.iloc[i], 0, 1)
 
-        expected_ret = df_pos.iloc[-1].values
-            
-        return np.array(expected_ret)
+        self.df_pos = df_pos
+    
+    def expected_return(self, pos_date: datetime) -> np.ndarray:
+        expected_ret = np.array(self.df_pos.loc[:pos_date].iloc[-1])
+
+        return expected_ret
     
--- strategy_v2/Strategy/MVO/AlphaModel/RSI.py
+++ strategy_v2/Strategy/MVO/AlphaModel/RSI.py
@@ -2,7 +2,6 @@ from pandas.core.api import DataFrame as DataFrame
 import numpy as np
 from datetime import datetime
 from strategy_v2.Strategy.MVO import AlphaModel
-from utils.data_helper import add_bday
 from utils.ta import rsi
 
 
@@ -22,41 +21,42 @@ class RSI(AlphaModel):
 
     def __str__(self) -> str:
         return f"RSI({self.rsi_threshold},{self.rsi_windows})"
-    
-    def expected_return(self, pos_date: datetime) -> np.ndarray:
 
-        lookback_end = add_bday(pos_date, -1)
-        expected_ret = []
+    def preprocess_data(self, data):
+        super().preprocess_data(data)        
 
         df = self.data['px']
-        df = df[:lookback_end]
-        df = df.tail(200)
 
-        assert max(df.index) < pos_date, 'Optimization has lookahead bias'
+        # signals on T is based on T-1 Close and before
+        df_close = df['Close'].shift(1)
 
-        df_close = df['Close']        
         df_rsi = rsi(df_close, window=self.rsi_windows)
         df_200ma = df_close.rolling(200).mean()
         df_5ma = df_close.rolling(5).mean()
+        df_ret = df_close.pct_change().rolling(self.rsi_windows).mean().abs().clip(0, 1)
 
         # for RSI threshold < 50, we treat this as buy strategy
         # for RSI threshold > 50, we treat this as sell strategy (still return a positive number)
+        # use rolling average abs returns to determine size of the signals
         if self.rsi_threshold < 50:
-            df_entry = 1 * ((df_rsi < self.rsi_threshold) & (df_close > df_200ma))
+            df_entry = df_ret * ((df_rsi < self.rsi_threshold) & (df_close > df_200ma))
             df_exit = -1 * (df_close > df_5ma)
         else:
-            df_entry = 1 * ((df_rsi > self.rsi_threshold) & (df_close > df_200ma))
+            df_entry = df_ret * ((df_rsi > self.rsi_threshold) & (df_close > df_200ma))
             df_exit = -1 * (df_close < df_5ma)
-
-        df_sig = df_entry + df_exit
+        
+        df_sig = (df_entry + df_exit).fillna(0)
         df_pos = df_sig.copy()
 
         # re-create the position based on signals
         for i in range(1, len(df_pos)):
             df_pos.iloc[i] = df_pos.iloc[i] + df_pos.iloc[i-1]            
-            df_pos.iloc[i] = np.clip(df_pos.iloc[i], 0, 1)            
+            df_pos.iloc[i] = np.clip(df_pos.iloc[i], 0, 1)    
 
-        expected_ret = df_pos.iloc[-1].values
+        self.df_pos = df_pos
+    
+    def expected_return(self, pos_date: datetime) -> np.ndarray:                
+        expected_ret = np.array(self.df_pos.loc[:pos_date].iloc[-1])
             
-        return np.array(expected_ret)
+        return expected_ret
     
